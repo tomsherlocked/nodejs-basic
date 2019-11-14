a152f5e54e1caea591ad1505da11a5b77e72aa77
TSherloc <TSherloc@uk.ibm.com>
2019-11-14 11:42:49 +0000

    added logs

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const express = require("express"),
   launcher = require("rocket-server-log");
 
 const env = process.env.ENV;
+console.log(`env; ${env}`);
 app.use(express.static(__dirname + "/public"));
 
 // create server
 const server = http.createServer(app);
 
 app.get("/env-api", function(req, res) {
+  console.log(`env; ${env}`);
   res.send(`This is your super secret environment variable: ${env}!`);
 });
 
